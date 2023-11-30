302c6ff564efe327ae8324cf6493010da4aec8ef
phurhard <phurhardeen@gmail.com>
2023-11-30 17:09:04 +0100

    tasks 6, using allSettled

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.forEach((result) => [
+      {
+        status: result.status,
+        value: result.value,
+      },
+    ]));
+}
