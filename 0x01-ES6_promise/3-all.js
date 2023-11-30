ad0e23374d1bcf95820fe81ff97285305efb85d5
phurhard <phurhardeen@gmail.com>
2023-11-30 15:15:46 +0100

    jji

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,6 +3,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => console.log(values[0].body, values[1].firstName, values[1].lastName))
+    .then(([values, u]) => console.log(values.body, u.firstName, u.lastName))
     .catch(() => console.log('Signup system offline'));
 }
