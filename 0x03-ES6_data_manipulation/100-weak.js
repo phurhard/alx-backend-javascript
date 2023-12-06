994367d2dad031d5cbf5203ad8962e7055a4c321
phurhard <phurhardeen@gmail.com>
2023-12-06 05:58:39 +0100

    weakMap is good for garbage collection

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,6 +2,7 @@ export const weakMap = new WeakMap();
 export default function queryAPI(endpoint) {
   let count = weakMap.get(endpoint) || 0;
   count += 1;
-  weakMap.set(endpoint, count);
+  
   if (count >= 5) throw new Error('Endpoint load is high');
+  weakMap.set(endpoint, count);
 }
