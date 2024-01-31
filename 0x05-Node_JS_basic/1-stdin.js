b1259cc1d32e4e5eaf6b056285ae310a5f46e8cc
phurhard <phurhardeen@gmail.com>
2024-01-31 03:31:08 +0100

    folder issue

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,17 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (data) => {
+  const input = data.toString().trim();
+
+  if (input.toLowerCase() === 'exit') {
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  }
+
+  process.stdout.write(`Your name is: ${input}\n`);
+});
+
+process.stdin.once('end', () => {
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
+});
